75ac0cd80d5a749a16b8cc208a165e5d65b291c4
Elioenai Roberto <42978220+LeoniSZ@users.noreply.github.com>
2022-03-14 17:45:02 -0300

    Add files via upload

---
+++ projeto_1/src/test/java/com/example/projeto_1/Projeto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
